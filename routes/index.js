fb715a0beba86efae4e93ca54e47d083cdba6142
Wil1i <williams.ca001@gmail.com>
2023-03-28 13:00:18 +0430

    New route added

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const indexController = require("../controllers/indexController")
 Router.get("/", indexController.get)
 Router.post("/", indexController.post)
 
+const newShopController = require("../controllers/newShopController")
+Router.get("/shops", newShopController.get)
+Router.post("/shops", newShopController.post)
+
 module.exports = Router
