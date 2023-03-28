46b93a5940957c64e49701648f859920841828c7
Wil1i <williams.ca001@gmail.com>
2023-03-28 10:55:18 +0430

    Unused files deleted

---
+++ controllers/indexController.js
@@ -0,0 +1,12 @@
+const get = (req, res) => {
+
+}
+
+const post = (req, res) => {
+
+}
+
+module.exports = {
+    get,
+    post
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const Router = express.Router()
 
+const indexController = require("../controllers/indexController")
+Router.get("/", indexController.get)
+Router.post("/", indexController.post)
+
 const adminLoginController = require("../controllers/adminLoginController")
 Router.get("/login", isNotLoggedIn,adminLoginController.get)
 Router.post("/login", isNotLoggedIn,adminLoginController.post, adminLoginController.loginSuccess)
