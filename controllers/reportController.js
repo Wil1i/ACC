c5207af476f04c3a043b7523f8c4b79db74467af
Wil1i <williams.ca001@gmail.com>
2023-03-30 10:24:41 +0430

    Report page added and completed

---
+++ controllers/reportController.js
@@ -0,0 +1,22 @@
+const Order = require("../models/Order")
+const Shop = require("../models/Shop")
+
+const get = async (req, res) => {
+    const orders = await Order.findAll()
+    const allShops = await Shop.findAll()
+    res.render("report", {
+        flash : req.flash(),
+        orders,
+        allShops,
+        q : false
+    })
+}
+
+const post = (req, res) => {
+
+}
+
+module.exports = {
+    get,
+    post
+}
