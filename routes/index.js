dcf98ba3ace3192cfcea98e123dc64dc4af62882
Wil1i <williams.ca001@gmail.com>
2023-03-30 10:27:04 +0430

    del url support

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,11 @@ const newShopController = require("../controllers/newShopController")
 Router.get("/shops", newShopController.get)
 Router.post("/shops", newShopController.post)
 
+const reportController = require("../controllers/reportController")
+Router.get("/report", reportController.get)
+Router.post("/report", reportController.post)
+
+const delApiController = require("../controllers/delApiController")
+Router.post("/del", delApiController.post)
+
 module.exports = Router
