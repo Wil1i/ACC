7ffb1a353a2fe096418dc2f80eaed4ac2e47e8a4
Wil1i <williams.ca001@gmail.com>
2023-04-05 17:33:56 +0430

    Confirm added | Submit button added

--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -2,6 +2,9 @@ jalaliDatepicker.startWatch();
 var newForm = document.getElementById("new-form")
 var submitBtn = document.getElementById("submitbtn")
 var delHandler = document.querySelectorAll(".delHandler")
+var orderId = document.getElementById("orderId")
+var price = document.getElementById("price")
+var submitForm = document.getElementById("submitForm")
 
 for(let i = 0; i < delHandler.length; i++){
     delHandler[i].addEventListener("dblclick", () => {
@@ -15,6 +18,16 @@ for(let i = 0; i < delHandler.length; i++){
 
 newForm.addEventListener("keydown", (key) => {
     if(key.key == "Enter"){
+        if(confirm(`شماره سفارش ${orderId.value} به مبلغ ${price.value} تومان ثبت شود؟`)){
+            submitBtn.click()
+        }else{
+            key.preventDefault()
+        }
+    }
+})
+
+submitForm.addEventListener("click", () => {
+    if(confirm(`شماره سفارش ${orderId.value} به مبلغ ${price.value} تومان ثبت شود؟`)){
         submitBtn.click()
     }
 })
