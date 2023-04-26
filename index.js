ac18b5e41c936496cf1e4f16c8ee73348fde43cb
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-04-26 16:24:29 +0330

    Def checker enabled

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const flash = require("connect-flash");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+require("./helpers/defaultChecker.js")();
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
 app.use(cookieParser());
-app.use(session({secret: config.app.secret}));
+app.use(session({ secret: config.app.secret }));
 app.use(express.static(__dirname + "/public"));
 app.use(morgan("combined"));
 
