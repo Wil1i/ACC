43f6c4403bb7d9065e877e542044f37c141289db
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-05-03 17:09:25 +0330

    Some edits and code cleans

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
-const express = require("express")
-const Router = express.Router()
+const express = require("express");
+const Router = express.Router();
 
-const indexController = require("../controllers/indexController")
-Router.get("/", indexController.get)
-Router.post("/", indexController.post)
+const indexController = require("../controllers/indexController");
+Router.get("/", indexController.get);
+Router.post("/", indexController.post);
 
-const newShopController = require("../controllers/newShopController")
-Router.get("/shops", newShopController.get)
-Router.post("/shops", newShopController.post)
+const newShopController = require("../controllers/newShopController");
+Router.get("/shops", newShopController.get);
+Router.post("/shops", newShopController.post);
 
-const reportController = require("../controllers/reportController")
-Router.get("/report", reportController.get)
-Router.post("/report", reportController.post)
+const reportController = require("../controllers/reportController");
+Router.get("/report", reportController.get);
+Router.post("/report", reportController.post);
 
-const delApiController = require("../controllers/delApiController")
-Router.post("/del", delApiController.post)
+const delApiController = require("../controllers/delApiController");
+Router.post("/del", delApiController.post);
 
-module.exports = Router
+const submitAPIController = require("../controllers/submitAPIController");
+Router.post("/api/orders/create", submitAPIController.post);
+
+const submitPriceController = require("../controllers/submitPriceController");
+Router.post("/submitprice", submitPriceController.post);
+
+module.exports = Router;
