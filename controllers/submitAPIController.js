7e7b788c36683b207360d10517b557f7b0a57ed5
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-05-03 17:09:17 +0330

    Submit price system added

---
+++ controllers/submitAPIController.js
@@ -0,0 +1,22 @@
+const Order = require("../models/Order");
+const axios = require("axios");
+
+const post = async (req, res) => {
+  const [orderId, supplier, description] = req.body;
+  const time = await axios.get("https://prayer.aviny.com/api/prayertimes/1");
+  const date = time.split(" - ")[0];
+
+  Order.create({
+    name: supplier,
+    orderId,
+    time: date,
+    description,
+    mode: "sabtFaktor",
+  }).then(() => {
+    res.json({ ok: true });
+  });
+};
+
+module.exports = {
+  post,
+};
--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -1,33 +1,56 @@
 jalaliDatepicker.startWatch();
-var newForm = document.getElementById("new-form")
-var submitBtn = document.getElementById("submitbtn")
-var delHandler = document.querySelectorAll(".delHandler")
-var orderId = document.getElementById("orderId")
-var price = document.getElementById("price")
-var submitForm = document.getElementById("submitForm")
+var newForm = document.getElementById("new-form");
+var submitBtn = document.getElementById("submitbtn");
+var delHandler = document.querySelectorAll(".delHandler");
+var orderId = document.getElementById("orderId");
+var price = document.getElementById("price");
+var submitForm = document.getElementById("submitForm");
+var submitPriceInputs = document.querySelectorAll(".submitPrice");
 
-for(let i = 0; i < delHandler.length; i++){
-    delHandler[i].addEventListener("dblclick", () => {
-        if(confirm(`سند شماره ${delHandler[i].innerHTML} حذف شود؟`)){
-            axios.post("/del", {id : delHandler[i].innerHTML}).then(() => {
-                location.reload()
-            })
-        }
-    })
+for (let i = 0; i < delHandler.length; i++) {
+  delHandler[i].addEventListener("dblclick", () => {
+    if (confirm(`سند شماره ${delHandler[i].innerHTML} حذف شود؟`)) {
+      axios.post("/del", { id: delHandler[i].innerHTML }).then(() => {
+        location.reload();
+      });
+    }
+  });
 }
 
 newForm.addEventListener("keydown", (key) => {
-    if(key.key == "Enter"){
-        if(confirm(`شماره سفارش ${orderId.value} به مبلغ ${price.value} تومان ثبت شود؟`)){
-            submitBtn.click()
-        }else{
-            key.preventDefault()
-        }
+  if (key.key == "Enter") {
+    if (
+      confirm(
+        `شماره سفارش ${orderId.value} به مبلغ ${price.value} تومان ثبت شود؟`
+      )
+    ) {
+      submitBtn.click();
+    } else {
+      key.preventDefault();
     }
-})
+  }
+});
 
 submitForm.addEventListener("click", () => {
-    if(confirm(`شماره سفارش ${orderId.value} به مبلغ ${price.value} تومان ثبت شود؟`)){
-        submitBtn.click()
-    }
-})
+  if (
+    confirm(
+      `شماره سفارش ${orderId.value} به مبلغ ${price.value} تومان ثبت شود؟`
+    )
+  ) {
+    submitBtn.click();
+  }
+});
+
+for (let i = 0; i < submitPriceInputs.length; i++) {
+  submitPriceInputs[i].addEventListener("dblclick", async () => {
+    try {
+      if (confirm("آيا از ثبت مبلغ مطمئن هستید‌؟")) {
+        await axios.post("/submitprice", {
+          id: submitPriceInputs[i].id,
+          price: submitPriceInputs[i].value,
+        });
+      }
+    } catch (error) {}
+    location.reload();
+  });
+}
